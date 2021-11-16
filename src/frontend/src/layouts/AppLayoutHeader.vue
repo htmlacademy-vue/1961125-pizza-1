e5132f52694c9a168ce1a4b1f4794a2f0541ece6
Amir Gafurov <gafurovamir@gmail.com>
2021-11-16 19:40:59 +0400

    MODULE 1: черновики компонентов

---
+++ src/frontend/src/layouts/AppLayoutHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <a class="logo">
+        <img
+          src="@/assets/img/logo.svg"
+          alt="VueWork logo"
+          width="147"
+          height="23"
+        />
+      </a>
+    </div>
+    <div>
+      <form action="#" class="header__search">
+        <input type="search" name="search" required placeholder="Поиск" />
+        <button type="submit">Найти</button>
+      </form>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppLayoutHeader",
+};
+</script>
+
+<style lang="scss" scoped></style>
