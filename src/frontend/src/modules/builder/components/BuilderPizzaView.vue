adebadc96b99b6032f707836e69740070ba3d955
Amir Gafurov <gafurovamir@gmail.com>
2021-11-20 10:08:58 +0400

    Доработка отображения слоев пиццы

--- src/frontend/src/modules/builder/components/BuilderPizzaView.vue
+++ src/frontend/src/modules/builder/components/BuilderPizzaView.vue
@@ -5,16 +5,22 @@
     @dragover.prevent
   >
     <div class="pizza__wrapper">
-      <div
-        v-for="(count, ingredient, index) in clearedIngredients"
-        :key="`${ingredient}-${index}`"
-        :class="[
-          'pizza__filling',
-          `pizza__filling--${ingredient}`,
-          { 'pizza__filling--second': count === 2 },
-          { 'pizza__filling--third': count === 3 },
-        ]"
-      ></div>
+      <template v-for="(count, ingredient, index) in clearedIngredients">
+        <div
+          :key="`${ingredient}-${index}`"
+          :class="getFillingClass(ingredient)"
+        ></div>
+        <div
+          v-if="count >= 2"
+          :key="`${ingredient}-${index}-second`"
+          :class="getFillingClass(ingredient, 2)"
+        ></div>
+        <div
+          v-if="count >= 3"
+          :key="`${ingredient}-${index}-third`"
+          :class="getFillingClass(ingredient, 3)"
+        ></div>
+      </template>
     </div>
   </div>
 </template>
@@ -48,6 +54,16 @@ export default {
       const type = e.dataTransfer.getData("ingredientType");
 
       this.$eventBus.$emit("increase-ingredient", type);
+    },
+    getFillingClass(ingredient, count = 1) {
+      const secondClass = "pizza__filling--second";
+      const thirdClass = "pizza__filling--third";
+      const classes = ["pizza__filling", `pizza__filling--${ingredient}`];
+
+      if (count >= 2) classes.push(secondClass);
+      if (count >= 3) classes.push(thirdClass);
+
+      return classes;
     },
   },
 };
